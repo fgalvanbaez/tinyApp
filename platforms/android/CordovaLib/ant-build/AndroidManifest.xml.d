3b37c97d432c7cc155055081cd79c16cefcd3ea2
Francisco A Galván Báez <francisco.galvan.baez@gmail.com>
2015-06-02 15:07:11 +0000

    Añado el index.html y el index.js

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Ubuntu\Computacion_Ubicua\tinyApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\Ubuntu\Computacion_Ubicua\tinyApp\platforms\android\CordovaLib\AndroidManifest.xml \
