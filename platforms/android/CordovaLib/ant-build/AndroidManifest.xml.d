45a292c750785378f80fb803bc0b8bde3671be47
Francisco A Galván Báez <francisco.galvan.baez@gmail.com>
2015-06-02 15:59:37 +0000

    Añados algunos ficheros

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-D:\Ubuntu\Computacion_Ubicua\tinyApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\Ubuntu\Computacion_Ubicua\tinyApp\platforms\android\CordovaLib\AndroidManifest.xml \
+C:\Users\FranciscoAlexis\Documents\tinyApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\FranciscoAlexis\Documents\tinyApp\platforms\android\CordovaLib\AndroidManifest.xml \
